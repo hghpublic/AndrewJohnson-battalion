0fe331f066ce08c06a4685925b88a4d00767121e
Heinz-Günter Hermanns <hgh.dev@t-online.de>
2025-11-21 16:59:52 +0000

    Added run script.

---
+++ run.sh
@@ -0,0 +1,20 @@
+#!/bin/sh
+
+git clean -dxf
+git reset --hard HEAD
+
+DATA_DIR="battalion.data"
+mkdir $DATA_DIR
+touch $DATA_DIR/battalion.sho
+touch battalion_hiscore
+cp -r DATA $DATA_DIR/
+cp -r MUSIC $DATA_DIR/
+cp -r SOUNDS $DATA_DIR/
+cp -r TEXTURES $DATA_DIR/
+
+make
+
+
+BATTALIONSCOREDIR="$(dirname "$0")"
+export BATTALIONSCOREDIR
+./battalion
