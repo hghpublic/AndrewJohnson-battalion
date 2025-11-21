c4eaec573525f3d5545d51970fe4dd62cc9b6dee
Heinz-Günter Hermanns <hgh.dev@t-online.de>
2025-11-21 16:59:52 +0000

    Added run script.

---
+++ run.sh
@@ -0,0 +1,25 @@
+#!/bin/sh
+
+git clean -dxf
+git reset --hard HEAD
+
+DATA_DIR="battalion.data"
+mkdir $DATA_DIR
+touch $DATA_DIR/battalion.sho
+touch battalion_hiscore
+cp -r DATA $DATA_DIR/
+cp -r MUSIC $DATA_DIR/
+cp -r SOUNDS $DATA_DIR/
+cp -r TEXTURES $DATA_DIR/
+
+make
+
+
+BATTALIONSCOREDIR="$(dirname "$0")"
+export BATTALIONSCOREDIR
+
+# Set AddressSanitizer suppression file
+SUPP_PATH="$(pwd)/battalion.supp"
+export LSAN_OPTIONS="suppressions=${SUPP_PATH}:print_suppressions=1"
+
+./battalion
